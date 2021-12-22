7c7051ebf157cb90dfe8eca91b8ff88ec0dc2603
jun <2190165626@qq.com>
2021-12-23 06:03:18 +0800

    更改了生成词云树的文件

--- testCloud.py
+++ testCloud.py
@@ -1,8 +1,5 @@
-# -*- coding = utf-8 -*-
-# @Time ： 2020/4/24 16:49
-# @Author : Alworm
-# @File : testCloud.py
-# @Software : PyCharm
+# 生成词云的图片
+
 import jieba
 from matplotlib import pyplot as plt    # 绘图，数据可视化
 from wordcloud import WordCloud
@@ -17,29 +14,23 @@ data = cur.execute(sql)
 text = ""
 for item in data:
     text = text + item[0]
-print(text)
 cur.close()
 conn.close()
-
-
 # 分词
 cut = jieba.cut(text)
 string = ' '.join(cut)
-print(len(string))
 
 img = Image.open(r'.\static\assets\img\tree.jpg')
 img_array = np.array(img)   # 将图片转换为数组
 wc = WordCloud(
     background_color='white',
     mask=img_array,
+    font_path="simhei.ttf"        
 )
 wc.generate_from_text(string)
-
-
 # 绘制图片
-
 fig = plt.figure(1)
 plt.imshow(wc)
 plt.axis('off')     # 不显示坐标轴
-# plt.show()          # 显示生成的词云
-plt.savefig(r'.\static\assets\img\word.jpg', dpi=500)
+# plt.savefig(r'.\static\assets\img\word.jpg', dpi=500)   #保存图片
+plt.show()          # 显示生成的词云
